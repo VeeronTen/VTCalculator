86e28560bb97e8e621a1488e94da8b5ca08b4203
Veeron Ten <veeronten@Veerons-iMac.local>
2017-11-22 10:40:52 +0300

    - interface - calculating

--- VTCalculator/ViewController.swift
+++ VTCalculator/ViewController.swift
@@ -1,25 +1,71 @@
-//
-//  ViewController.swift
-//  VTCalculator
-//
-//  Created by Veeron Ten on 11/22/17.
-//  Copyright © 2017 Veeron Ten. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textOutput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textOutput.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func digitPressed(_ sender: Any) {
+        if let digitToAppend = (sender as! UIButton).currentTitle{
+            textOutput.text?.append(digitToAppend)
+        }
+    }
+    
+    @IBAction func operationPressed(_ sender: Any) {
+        if let operatorToAppend = (sender as! UIButton).currentTitle?.first{
+            if((operatorToAppend=="*" || operatorToAppend=="/") && (textOutput.text == "" || textOutput.text=="+" || textOutput.text=="-")){
+                //do nothing: mult and div can not be the first operation
+            }else if (!(textOutput.text?.isEmpty)! && (textOutput.text?.last?.isOperation())!){
+                //replace last
+                textOutput.text?.removeLast()
+                textOutput.text?.append(operatorToAppend)
+            }else{
+                //just append
+                textOutput.text?.append(operatorToAppend)
+            }
+        }
+    }
+    
+    //we can get SIGABRT here if the statement is not correct
+    @IBAction func equalsPressed(_ sender: Any) {
+        var toOutput: String = ""
+        if textOutput.text == ""{
+            return
+        }
+        let expression:NSExpression? =  NSExpression(format: (textOutput.text)!)
+        if let answer = expression?.expressionValue(with: nil, context: nil) as? Double{
+            if(answer.canBeSimplified()){
+                toOutput = Int(answer).description
+            }else{
+                toOutput = answer.description
+            }
+        }
+        textOutput.text = toOutput
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        textOutput.text = ""
+    }
 }
 
+extension Double{
+    //has the instance got digits after the point?
+    func canBeSimplified() -> Bool{
+        return floor(self) - self == 0
+    }
+}
+
+extension Character{
+    func isOperation() -> Bool{
+        let operations = [Character](arrayLiteral: "+", "-", "*", "/")
+        return operations.contains(self)
+    }
+}
